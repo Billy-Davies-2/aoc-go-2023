17ec29b7a5baa88c5c0c4ee7520060000e2f8e7d
Billy Davies <13783427+Billy-Davies-2@users.noreply.github.com>
2023-12-10 09:02:29 -0500

    initial bootstrap of the advent of code cli app

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Billy-Davies-2/aoc-go-2023
+
+go 1.21
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Billy-Davies-2/aoc-go-2023/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
